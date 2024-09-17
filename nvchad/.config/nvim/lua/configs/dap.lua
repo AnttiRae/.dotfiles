63281cee8b0e68c8f9a7baa3b7fc5bd6df37ac64
Antti Rae <anttirae@gmail.com>
2024-09-17 19:31:37 +0300

    linters and formatters

---
+++ nvchad/.config/nvim/lua/configs/dap.lua
@@ -0,0 +1,6 @@
+local dap = require("dap")
+
+dap.adapters.python = {
+  type = "executable",
+  command = ""
+}
--- nvchad/.config/nvim/lua/plugins/init.lua
+++ nvchad/.config/nvim/lua/plugins/init.lua
@@ -1,13 +1,85 @@
 return {
+
+  "mfussenegger/nvim-lint",
+  event = {
+    "BufReadPre",
+    "BufNewFile",
+  },
+  config = function()
+    local lint = require "lint"
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
+      python = { "pylint" },
+    }
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+    keys = {
+      {
+        "<leader>l",
+        function()
+          lint.try_lint()
+        end,
+        desc = "Trigger linting for current file",
+      },
+    }
+  end,
+
   {
-    "mbbill/undotree", lazy = false
+    "mbbill/undotree",
+    lazy = false,
   },
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
-      require "configs.conform"
+      local conform = require "conform"
+      conform.setup {
+        formatters_by_ft = {
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
+          svelte = { "prettier" },
+          css = { "prettier" },
+          html = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+          graphql = { "prettier" },
+          lua = { "stylua" },
+          python = { "isort", "black" },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        },
+      }
     end,
+    keys = {
+      {
+        "<leader>mp",
+        function()
+          require("conform").format {
+            lsp_fallback = true,
+            async = false,
+            timeout_ms = 500,
+          }
+        end,
+        desc = "Format file or range (in visual mode)",
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
@@ -19,68 +91,77 @@ return {
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
   },
   {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    -- calling `setup` is optional for customization
-    require("fzf-lua").setup({})
-  end
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- calling `setup` is optional for customization
+      require("fzf-lua").setup {}
+    end,
   },
   {
-  'rmagatti/auto-session', lazy = false,
-  config = function()
-    require("auto-session").setup {
-      log_level = "error",
-      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-    }
-  end
+    "rmagatti/auto-session",
+    lazy = false,
+    config = function()
+      require("auto-session").setup {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      }
+    end,
   },
   {
-  'linux-cultist/venv-selector.nvim', lazy = true,
-  dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-  opts = {
-    -- Your options go here
-    -- name = "venv"
-    name = {"venv", ".venv", "env"}
-    -- auto_refresh = false
-  },
-  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-  keys = {
-    -- Keymap to open VenvSelector to pick a venv.
-    { '<leader>vs', '<cmd>VenvSelect<cr>' },
-    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-  }
+    "linux-cultist/venv-selector.nvim",
+    lazy = true,
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    opts = {
+      -- Your options go here
+      -- name = "venv"
+      name = { "venv", ".venv", "env" },
+      -- auto_refresh = false
+    },
+
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { "<leader>vs", "<cmd>VenvSelect<cr>" },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+    },
   },
   {
-  "williamboman/mason.nvim",
+    "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
         "lua-language-server",
         "stylua",
         "html-lsp",
-        "css-lsp" ,
+        "css-lsp",
         "prettier",
         "ruff-lsp",
         "ruff",
         "debugpy",
-        "pyright",
         "flake8",
+        "pylint",
+        "eslint_d",
         "black",
-        "autopep8"
+        "isort",
+        "autopep8",
+        "golangci-lint",
       },
     },
-   },
+  },
   {
-  "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "vim", "lua", "vimdoc",
-       "html", "css"
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
       },
     },
   },
@@ -88,41 +169,106 @@ return {
     "tris203/precognition.nvim",
     event = "VeryLazy",
     config = {
-    startVisible = true,
-    showBlankVirtLine = true,
-    highlightColor = { link = "Comment" },
-    hints = {
-         Caret = { text = "^", prio = 2 },
-         Dollar = { text = "$", prio = 1 },
-         MatchingPair = { text = "%", prio = 5 },
-         Zero = { text = "0", prio = 1 },
-         w = { text = "w", prio = 10 },
-         b = { text = "b", prio = 9 },
-         e = { text = "e", prio = 8 },
-         W = { text = "W", prio = 7 },
-         B = { text = "B", prio = 6 },
-         E = { text = "E", prio = 5 },
-    },
-    gutterHints = {
+      startVisible = true,
+      showBlankVirtLine = true,
+      highlightColor = { link = "Comment" },
+      hints = {
+        Caret = { text = "^", prio = 2 },
+        Dollar = { text = "$", prio = 1 },
+        MatchingPair = { text = "%", prio = 5 },
+        Zero = { text = "0", prio = 1 },
+        w = { text = "w", prio = 10 },
+        b = { text = "b", prio = 9 },
+        e = { text = "e", prio = 8 },
+        W = { text = "W", prio = 7 },
+        B = { text = "B", prio = 6 },
+        E = { text = "E", prio = 5 },
+      },
+      gutterHints = {
         G = { text = "G", prio = 10 },
         gg = { text = "gg", prio = 9 },
         PrevParagraph = { text = "{", prio = 8 },
         NextParagraph = { text = "}", prio = 8 },
-      }
+      },
     },
   },
   {
-      "OXY2DEV/markview.nvim",
-      lazy = false,      -- Recommended
-      -- ft = "markdown" -- If you decide to lazy-load anyway
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
 
-      dependencies = {
-          -- You will not need this if you installed the
-          -- parsers manually
-          -- Or if the parsers are in your $RUNTIMEPATH
-          "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
 
-          "nvim-tree/nvim-web-devicons"
-      }
-  }
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    recommended = true,
+    desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      -- virtual text for the debugger
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {},
+      },
+    },
+
+    -- stylua: ignore
+    keys = {
+      { "<leader>d", "", desc = "+debug", mode = {"n", "v"} },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+      { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+      { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+      { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+      { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+      { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+      { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+    },
+
+    config = function()
+      -- load mason-nvim-dap here, after all adapters have been setup
+      if LazyVim.has "mason-nvim-dap.nvim" then
+        require("mason-nvim-dap").setup(LazyVim.opts "mason-nvim-dap.nvim")
+      end
+
+      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+      for name, sign in pairs(LazyVim.config.icons.dap) do
+        sign = type(sign) == "table" and sign or { sign }
+        vim.fn.sign_define(
+          "Dap" .. name,
+          { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+        )
+      end
+
+      -- setup dap config by VsCode launch.json file
+      local vscode = require "dap.ext.vscode"
+      local json = require "plenary.json"
+      vscode.json_decode = function(str)
+        return vim.json.decode(json.json_strip_comments(str))
+      end
+
+      -- Extends dap.configurations with entries read from .vscode/launch.json
+      if vim.fn.filereadable ".vscode/launch.json" then
+        vscode.load_launchjs()
+      end
+    end,
+  },
 }
