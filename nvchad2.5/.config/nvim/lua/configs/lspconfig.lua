ba74a3160509be490ada059a3147815b720df5ef
Antti Rae <anttirae@gmail.com>
2024-10-16 21:04:12 +0300

    nvchad2.5 update

---
+++ nvchad2.5/.config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,40 @@
+-- EXAMPLE
+local nvlsp = require("nvchad.configs.lspconfig")
+local lspconfig = require("lspconfig")
+
+nvlsp.defaults() -- loads nvchad's defaults
+local servers = {
+	"html",
+	"cssls",
+	"gopls",
+	"mojo",
+	"ruff",
+	"pyright",
+	"lua_ls",
+}
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
+end
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
