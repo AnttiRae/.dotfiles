5d8f41740864f32609a779e54f3c7cc827fb8253
Antti Rae <anttirae@gmail.com>
2024-11-03 22:51:37 +0200

    add code outline plugin

--- nvchad2.5/.config/nvim/lua/plugins/init.lua
+++ nvchad2.5/.config/nvim/lua/plugins/init.lua
@@ -39,6 +39,17 @@ return {
 		cmd = { "RainbowTrails" },
 	},
 	{
+		"hedyhli/outline.nvim",
+		lazy = true,
+		cmd = { "Outline", "OutlineOpen" },
+		keys = { -- Example mapping to toggle outline
+			{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+		},
+		opts = {
+			-- Your setup opts here
+		},
+	},
+	{
 		"mbbill/undotree",
 		lazy = false,
 	},
