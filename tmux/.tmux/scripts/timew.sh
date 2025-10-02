b127319689da884d662fabb2ea67f08349fd36d1
Antti Rae <antti.rae@anders.com>
2025-10-02 18:26:14 +0300

    timew script exit if no timew installed

--- tmux/.tmux/scripts/timew.sh
+++ tmux/.tmux/scripts/timew.sh
@@ -1,3 +1,9 @@
+if ! command -v timew >/dev/null 2>$1
+then
+    echo "timew not found"
+    exit 0
+fi
+
 tag=$(timew tags | tail -n +4 | awk '!/^[[:space:]]*$/' | cut -d " " -f 1 | fzf --tmux center,20%,20%)
 if [ -n "$tag" ]; then
   timew start "$tag" &>/dev/null
