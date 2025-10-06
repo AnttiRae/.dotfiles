6bab01aa4410ea2aca6f3a66a5cf90e22fa012eb
Antti Rae <antti.rae@anders.com>
2025-10-06 19:29:02 +0300

    nvim helm-ls lsp tree sitter

--- kickstart-nvim/.config/nvim/init.lua
+++ kickstart-nvim/.config/nvim/init.lua
@@ -694,6 +694,15 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
+        helm_ls = {
+          settings = {
+            ['helm-ls'] = {
+              yamlls = {
+                path = 'yaml-language-server',
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -1020,7 +1029,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'helm' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
