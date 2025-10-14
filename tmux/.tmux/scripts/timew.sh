4c3b35fffa8bcd2364568f943ef5d50727bd6f4a
Antti Rae <antti.rae@anders.com>
2025-10-14 16:49:57 +0300

    tt

--- tmux/.tmux/scripts/timew.sh
+++ tmux/.tmux/scripts/timew.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 jira_tags=$(jira-issues)
 local_tags=$(timew tags | tail -n +4 | awk '!/^[[:space:]]*$/' | cut -d " " -f 1)
-all_tags="${jira_tags}/n${local_tags}"
-tag=$(echo "$all_tags" | awk -F: '!seen[$1]++' | fzf --tmux center,30%,40%)
+tags="${jira_tags}/n${local_tags}"
+env && echo "\n jira tags: $jira_tags"
+tag=$(echo "$tags" | awk -F: '!seen[$1]++' | fzf --tmux center,30%,40%)
 if [ -n "$tag" ]; then
-    timew start "$(echo "$tag" | cut -d ":" -f 1)" &>/dev/null
+    timew start "$(echo $tag | cut -d ":" -f 1)" &>/dev/null
 fi
-
 
