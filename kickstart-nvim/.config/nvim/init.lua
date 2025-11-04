cadf53b1f51d1543d7d9589e5c7be3f782514739
Antti Rae <antti.rae@anders.com>
2025-11-04 20:39:21 +0200

    try devdocs with apidocs.nvim

--- kickstart-nvim/.config/nvim/init.lua
+++ kickstart-nvim/.config/nvim/init.lua
@@ -1077,7 +1077,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'helm', 'norg' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'helm' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
