958dc66d2802311d22367843295850d3cc940163
Antti Rae <anttirae@gmail.com>
2026-02-01 21:53:03 +0200

    atuin for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -54,3 +54,11 @@ if type -q pyenv
   [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init - zsh)"
 end
+
+## ATUIN
+set -gx ATUIN_NOBIND "true"
+atuin init fish | source
+
+# bind to ctrl-r in normal and insert mode, add any other bindings you want here too
+bind \cr _atuin_search
+bind -M insert \cr _atuin_search
