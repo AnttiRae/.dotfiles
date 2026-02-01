ef4e6bb5d310e46e2db0721ce8f2ed3b43ea55aa
Antti Rae <anttirae@gmail.com>
2026-02-01 14:01:44 +0200

    Fix neovim errors

--- kickstart-nvim/.config/nvim/init.lua
+++ kickstart-nvim/.config/nvim/init.lua
@@ -421,11 +421,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          preview = {
+            treesitter = false,
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
@@ -1084,7 +1084,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'helm' },
