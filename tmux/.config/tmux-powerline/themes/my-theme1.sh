e8c6ab0c6bbf7da9d38a5352146be50d03d28f56
Antti Rae <anttirae@gmail.com>
2026-02-04 21:40:15 +0200

    Tmux remove more useless stuff

--- tmux/.config/tmux-powerline/themes/my-theme1.sh
+++ tmux/.config/tmux-powerline/themes/my-theme1.sh
@@ -124,7 +124,7 @@ if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
     "pwd 0 35"
     #"macos_notification_count 29 255"
     "hostname 16 45"
-    #"mode_indicator 0 255"
+    # "mode_indicator 0 255"
     #"mailcount 9 255"
     # "now_playing 234 37"
     # "cpu 240 136"
