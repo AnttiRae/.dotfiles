d76b0b09fcac40e9e88ddba9ce126c0b60849d5b
Antti Rae <anttirae@gmail.com>
2026-02-04 19:24:45 +0200

    Delete tmux theme stuff

--- tmux/.config/tmux-powerline/themes/my-theme1.sh
+++ tmux/.config/tmux-powerline/themes/my-theme1.sh
@@ -125,7 +125,6 @@ if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
     #"macos_notification_count 29 255"
     "hostname 16 45"
     #"mode_indicator 0 255"
-    "timew"
     #"mailcount 9 255"
     # "now_playing 234 37"
     # "cpu 240 136"
