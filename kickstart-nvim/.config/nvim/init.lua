c75cd14ed4b4c962083a983fbe14d6beaa8b8be4
Antti Rae <anttirae@gmail.com>
2026-05-08 17:03:47 +0300

    neovim: remove useless plugins

--- kickstart-nvim/.config/nvim/init.lua
+++ kickstart-nvim/.config/nvim/init.lua
@@ -1170,7 +1170,29 @@ require('lazy').setup({
       })
     end,
   },
-
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    dependencies = { 'amansingh-afk/milli.nvim' },
+    opts = function()
+      local splash = require('milli').load { splash = 'fire' }
+      return {
+        dashboard = {
+          enabled = true,
+          sections = {
+            { section = 'header', text = table.concat(splash.frames[1], '\n') },
+            { section = 'keys', gap = 1, padding = 1 },
+            { section = 'startup' },
+          },
+        },
+      }
+    end,
+    config = function(_, opts)
+      require('snacks').setup(opts)
+      require('milli').snacks { splash = 'fire', loop = true }
+    end,
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
